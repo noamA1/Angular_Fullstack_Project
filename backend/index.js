2a0242e2be1986de28f8448347c5028cbfb2c773
Noam Amron <noam0574@gmail.com>
2022-07-24 22:10:00 +0300

    connect to cloud mongoDB

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 mongoose
-  .connect(`${dbConfig.url}/shop`)
+  .connect(`${dbConfig.url}`)
   .then(() => {
     console.log("Successfully connected to the database");
   })
